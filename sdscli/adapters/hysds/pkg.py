96cdb958fa11296700c35eaa8b0cab4a226a84d0
Gerald Manipon <pymonger@gmail.com>
2018-03-05 22:18:04 +0000

    full implementation of HySDS package export

--- sdscli/adapters/hysds/pkg.py
+++ sdscli/adapters/hysds/pkg.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 from __future__ import print_function
 
-import os, json, yaml, requests, traceback
+import os, json, yaml, requests, tarfile, shutil, traceback
 from fabric.api import execute, hide
 
 from prompt_toolkit.shortcuts import prompt, print_tokens
@@ -52,8 +52,9 @@ def export(args):
     cont_id = args.id
 
     # query for container
-    es_url = "http://{}:9200".format(conf.get('MOZART_ES_PVT_IP'))
-    hits = run_query(es_url, "containers", {
+    mozart_es_url = "http://{}:9200".format(conf.get('MOZART_ES_PVT_IP'))
+    grq_es_url = "http://{}:9200".format(conf.get('GRQ_ES_PVT_IP'))
+    hits = run_query(mozart_es_url, "containers", {
         "query": {
             "term": { "_id": cont_id }
         }
@@ -61,12 +62,13 @@ def export(args):
     if len(hits) == 0:
         logger.error("SDS package id {} not found.".format(cont_id))
         return 1
-    cont_info = hits[0]
+    cont_info = hits[0]['_source']
     logger.debug("cont_info: {}".format(json.dumps(cont_info, indent=2)))
 
     # set export directory
+    outdir = normpath(args.outdir)
     export_name = "{}.sdspkg".format(cont_id)
-    export_dir = normpath(os.path.join(args.outdir, export_name))
+    export_dir = os.path.join(outdir, export_name)
     logger.debug("export_dir: {}".format(export_dir))
 
     # if directory exists, stop
@@ -78,12 +80,70 @@ def export(args):
     validate_dir(export_dir)
 
     # download container
-    get(cont_info['_source']['url'], export_dir)
+    get(cont_info['url'], export_dir)
+    cont_info['url'] = os.path.basename(cont_info['url'])
 
     # query job specs
-    hits = run_query(es_url, "job_specs", {
+    job_specs = [i['_source'] for i in run_query(mozart_es_url, "job_specs", {
         "query": {
-            "term": { "container": cont_id }
+            "term": { "container.raw": cont_id }
         }
-    })
-    logger.debug("job_specs: {}".format(json.dumps(hits, indent=2)))
+    })]
+    logger.debug("job_specs: {}".format(json.dumps(job_specs, indent=2)))
+
+    # pull hysds_ios for each job_spec and download any dependency images
+    hysds_ios = []
+    dep_images = {}
+    for job_spec in job_specs:
+        # download dependency images
+        for d in job_spec.get('dependency_images', []):
+            if d['container_image_name'] in dep_images:
+                d['container_image_url'] = dep_images[d['container_image_name']]
+            else:
+                # download container
+                get(d['container_image_url'], export_dir)
+                d['container_image_url'] = os.path.basename(d['container_image_url'])
+                dep_images[d['container_image_name']] = d['container_image_url']
+
+
+        # collect hysds_ios from mozart
+        mozart_hysds_ios = [i['_source'] for i in run_query(mozart_es_url, "hysds_ios", {
+            "query": {
+                "term": { "job-specification.raw": job_spec['id'] }
+            }
+        })]
+        logger.debug("Found {} hysds_ios on mozart for {}.".format(len(mozart_hysds_ios), job_spec['id']))
+        hysds_ios.extend(mozart_hysds_ios)
+        
+        # collect hysds_ios from mozart
+        grq_hysds_ios = [i['_source'] for i in run_query(grq_es_url, "hysds_ios", {
+            "query": {
+                "term": { "job-specification.raw": job_spec['id'] }
+            }
+        })]
+        logger.debug("Found {} hysds_ios on grq for {}.".format(len(grq_hysds_ios), job_spec['id']))
+        hysds_ios.extend(grq_hysds_ios)
+    logger.debug("Found {} hysds_ios total.".format(len(hysds_ios)))
+
+    # clean out allowed accounts
+    for hysds_io in hysds_ios:
+        if 'allowed_accounts' in hysds_io:
+            del hysds_io['allowed_accounts']
+
+    # dump manifest JSON
+    manifest = {
+        "containers" : cont_info,
+        "job_specs": job_specs,
+        "hysds_ios": hysds_ios,
+    }
+    manifest_file = os.path.join(export_dir, 'manifest.json')
+    with open(manifest_file, 'w') as f:
+        json.dump(manifest, f, indent=2, sort_keys=True)
+
+    # tar up hysds package
+    tar_file = os.path.join(outdir, "{}.tar".format(export_name))
+    with tarfile.open(tar_file, "w") as tar:
+        tar.add(export_dir, arcname=os.path.relpath(export_dir, outdir))
+
+    # remove package dir
+    shutil.rmtree(export_dir)
